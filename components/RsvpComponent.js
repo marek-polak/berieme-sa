76a48f42b43591d0d081a9a15ae6e28874232642
Marek Polak <polak.marek@gmail.com>
2020-02-25 14:59:12 +0100

    styling

--- components/RsvpComponent.js
+++ components/RsvpComponent.js
@@ -1,11 +1,13 @@
-import React, {Fragment} from 'react';
-import ReactDOM from 'react-dom';
+import React, { Fragment } from "react";
+import ReactDOM from "react-dom";
 
 import "./RsvpComponent.css";
 
-// mongo stitch connection 
-const client = stitch.Stitch.initializeDefaultAppClient('berieme-sa-qdily');
-const db = client.getServiceClient(stitch.RemoteMongoClient.factory, 'mongodb-atlas').db('wedding');
+// mongo stitch connection
+const client = stitch.Stitch.initializeDefaultAppClient("berieme-sa-qdily");
+const db = client
+  .getServiceClient(stitch.RemoteMongoClient.factory, "mongodb-atlas")
+  .db("wedding");
 
 /*       client.auth.loginWithCredential(new stitch.AnonymousCredential()).then(user =>
         db.collection('guests').updateOne({owner_id: client.auth.user.id}, {$set:{number:42}}, {upsert:true})
@@ -21,15 +23,23 @@ const db = client.getServiceClient(stitch.RemoteMongoClient.factory, 'mongodb-at
 const Title = ({ name }) => {
   return (
     <Fragment>
-      <h1 className="rsvp__title"> R.S.V.P.</h1>
-      <h3> Ahoj {name}</h3>
-      <div className='info'>
-        <p>Radi by sme Ťa/Vás pozvali po obrade k stolu. Prosím pomôž nám vyplnením formuláru nižšie - pričom platí “čím skôr, tým lepšie.”</p>
-        <p>Formulár po odoslaní možeš kedykoľvek upraviť tým, že ho znovu odošleš /ak je to nutné, kľudne tak sprav, dozvieme sa o tom/.</p>
+      <div className="rsvp__intro">
+        <h1 className="rsvp__title"> R.S.V.P.</h1>
+        <h3> Ahoj {name}</h3>
+        <div className="info">
+          <p>
+            Radi by sme Ťa/Vás pozvali po obrade k stolu. Prosím pomôž nám
+            vyplnením formuláru nižšie - pričom platí “čím skôr, tým lepšie.”
+          </p>
+          <p>
+            Formulár po odoslaní možeš kedykoľvek upraviť tým, že ho znovu
+            odošleš /ak je to nutné, kľudne tak sprav, dozvieme sa o tom/.
+          </p>
+        </div>
       </div>
     </Fragment>
   );
-}
+};
 
 class RSVPComponent extends React.Component {
   constructor(props) {
@@ -37,44 +47,51 @@ class RSVPComponent extends React.Component {
 
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
-    this.state = { liked: false , urlParams};
+    this.state = { liked: false, urlParams };
   }
 
-  retrieveData(){
-    if(this.state.urlParams.has('uid')) {
-      const useruid = this.state.urlParams.get('uid');
-      
-      client.auth.loginWithCredential(new stitch.AnonymousCredential())
+  retrieveData() {
+    if (this.state.urlParams.has("uid")) {
+      const useruid = this.state.urlParams.get("uid");
+
+      client.auth
+        .loginWithCredential(new stitch.AnonymousCredential())
         .then(() =>
-          db.collection('guests').find({uid: useruid}, { limit: 100}).asArray()
-        ).then(docs => {
-            console.log("Found docs", docs)
-            console.log("[MongoDB Stitch] Connected to Stitch")
-            this.setState({user: docs[0]}); 
-        }).catch(err => {
-          console.error(err)
+          db
+            .collection("guests")
+            .find({ uid: useruid }, { limit: 100 })
+            .asArray()
+        )
+        .then(docs => {
+          console.log("Found docs", docs);
+          console.log("[MongoDB Stitch] Connected to Stitch");
+          this.setState({ user: docs[0] });
+        })
+        .catch(err => {
+          console.error(err);
         });
-      }
+    }
   }
-  
 
-  componentDidMount(){
+  componentDidMount() {
     this.retrieveData();
   }
 
   render() {
     if (this.state.liked) {
-      return 'You liked this.';
+      return "You liked this.";
     }
 
-    if(this.state.urlParams.has('uid')) {
-      return `Ahoj ${this.state.urlParams.get('uid')} - ${JSON.stringify(this.state.user)}`;
+    if (this.state.urlParams.has("uid")) {
+      return `Ahoj ${this.state.urlParams.get("uid")} - ${JSON.stringify(
+        this.state.user
+      )}`;
     }
 
     return (
       <Fragment>
-        <Title name="Zdenka"/>
-        <button onClick={() => this.setState({ liked: true }) }>
+        <Title name="Zdenka" />
+        <button onClick={() => this.setState({ liked: true })}>
           Likeaaaaa
         </button>
       </Fragment>
@@ -82,7 +99,6 @@ class RSVPComponent extends React.Component {
   }
 }
 
-
-const domContainer = document.querySelector('#components');
+const domContainer = document.querySelector("#components");
 //domContainer.style.display = 'none';
 ReactDOM.render(<RSVPComponent />, domContainer);
