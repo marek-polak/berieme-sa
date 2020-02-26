710e214ae18e9cbe9de682f1547136b24df43f69
Marek Polak <polak.marek@gmail.com>
2020-02-26 15:36:00 +0100

    installed scrollmagic & gsap via npm

--- webpack.config.js
+++ webpack.config.js
@@ -82,5 +82,12 @@ module.exports = {
         ],
       },
     ]
+  },
+
+  resolve: {
+    alias: {
+      "animation.gsap": path.resolve('node_modules', 'scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap.js'),
+    }
   }
+  
 }
