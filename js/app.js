0a4680d256c54da5ff250afb9b7da391d8879c7f
Marek <polak.marek@gmail.com>
2020-04-09 11:11:11 +0200

    integrated lottie & covid-text (#6)

--- js/app.js
+++ js/app.js
@@ -4,10 +4,12 @@ require('../css/fonts.css');
 require('../css/normalize.css');
 require('../css/scrollbar.css');
 require('../css/animation.css');
+require('../css/corona.scss');
 
 // JS
 window.$ = require('jquery');
 require('./main.js');
 require('./plugins.js');
 require('./animation.js');
+require('./lottie.js');
 require('../components/RsvpComponent.js');
