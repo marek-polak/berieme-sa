0aadc4c2fa4ee2f8af17a2319b71b0933e1982e3
Marek Polak <polak.marek@gmail.com>
2020-04-09 11:09:43 +0200

    integrated lottie & covid-text

---
+++ js/lottie.js
@@ -0,0 +1,10 @@
+
+import Lottie from 'lottie-web';
+
+var animation = Lottie.loadAnimation({
+  container: document.getElementById('lottie-bg'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'img/17977-corona-virus.json'
+})
